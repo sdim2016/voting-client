b2a7a922decc31b6277ac07c673219b715cd618a
Dmitrii Slepnev <m4872@student.jamk.fi>
2019-10-08 15:34:25 +0300

    Added login.php, authenticate.php, index.php - testing login algorithm and PHP sessions

---
+++ login.php
@@ -0,0 +1,78 @@
+<?php
+$username = null;
+$password = null;
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    if(!empty($_POST["username"]) && !empty($_POST["password"])) {
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+
+        if($username == 'user' && $password == 'password') {
+            session_start();
+            $_SESSION["authenticated"] = 'true';
+            header('Location: index.php');
+        }
+        else {
+            header('Location: login.php');
+        }
+
+    } else {
+        header('Location: login.php');
+    }
+} else {
+?>
+
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Login - Brand</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
+    <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
+</head>
+
+<body class="bg-gradient-primary">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-9 col-lg-6 col-xl-6">
+                <div class="card shadow-lg o-hidden border-0 my-5">
+                    <div class="card-body p-0">
+                        <div class="row">
+                            <div class="col-lg-12 col-xl-12">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h4 class="text-dark mb-4">Blockchain Voting App</h4>
+                                    </div>
+                                    <form class="user" method="post">
+                                        <div class="form-group"><input class="form-control form-control-user" type="text" name="username" placeholder="Username"></div>
+                                        <div class="form-group"><input class="form-control form-control-user" type="password" id="exampleInputPassword" placeholder="Password" name="password"></div>
+                                        <div class="form-group">
+                                            <div class="custom-control custom-checkbox small">
+                                                <div class="form-check"><input class="form-check-input custom-control-input" type="checkbox" id="formCheck-1"><label class="form-check-label custom-control-label" for="formCheck-1">Remember Me</label></div>
+                                            </div>
+                                        </div><button class="btn btn-primary btn-block text-white btn-user" type="submit">Login via XAMK</button>
+                                        <hr>
+                                        <p class="text-center">You can login to my Blockchain Voting application using your XAMK account.</p>
+                                        <hr>
+                                    </form>
+                                    <div class="text-center"><a class="small" href="forgot-password.html">Forgot Password?</a></div>
+                                    <div class="text-center"><a class="small" href="register.html">Create an Account!</a></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/chart.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.js"></script>
+    <script src="assets/js/script.min.js"></script>
+</body>
+
+</html>
