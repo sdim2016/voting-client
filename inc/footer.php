ef8f11871d2e818e7f7d73861d7afbde70f09dee
Dmitrii Slepnev <odmsl002@edu.xamk.fi>
2019-10-10 19:13:20 +0300

    Switched to header.php + footer.php logic.

---
+++ inc/footer.php
@@ -0,0 +1,15 @@
+</div>
+<footer class="bg-white sticky-footer">
+    <div class="container my-auto">
+        <div class="text-center my-auto copyright"><span>Copyright &#169; Dmitrii Slepnev 2019</span></div>
+    </div>
+</footer>
+</div><a class="border rounded d-inline scroll-to-top" href="#page-top"><i class="fas fa-angle-up"></i></a></div>
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+<script src="assets/js/chart.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.js"></script>
+<script src="assets/js/script.min.js"></script>
+</body>
+
+</html>
