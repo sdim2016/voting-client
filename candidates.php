52a448dbf85746d9e9d4b9ecc28e7a4285964cc2
Dmitrii Slepnev <odmsl002@edu.xamk.fi>
2019-10-15 21:16:41 +0300

    Chart is now adaptive + custom legend

--- candidates.php
+++ candidates.php
@@ -3,7 +3,7 @@ require_once 'authenticate.php';
 $page = 'candidates';
 include 'inc/header.php';
 
-$votesarr = array("Obama" => 10, "Trump" => 20, "Putin" => 30, "Medvedev" => 6, "Johnson" => 18);
+$votesarr = array("Obama" => 10, "Trump" => 20, "Putin" => 30, "Medvedev" => 6, "Johnson" => 18, "Zelensky" => 3);
 $sum = 0;
 foreach ($votesarr as $candidate => $votes) {
 $sum += $votes;
@@ -57,55 +57,98 @@ $sum += $votes;
   </div>
   </div>
 
-  <div class="col-md-4">
+  <div class="col-md-5">
 
 
 <div class="card shadow mb-5">
   <div class="card-header py-3">
     <p class="text-primary m-0 font-weight-bold">Chart</p>
   </div>
-  <div class="card-body text-center">
+  <div class="card-body">
         <div class="chart-area">
-            <canvas data-bs-chart="{&quot;type&quot;:&quot;doughnut&quot;,&quot;data&quot;:{&quot;labels&quot;:[<?php
-              $cnds = '';
-              foreach ($votesarr as $candidate => $votes) {
-                $cnds = $cnds.'&quot;'.$candidate.'&quot,';
-              }
-              $cnds = substr($cnds, 0, -1);
-              echo $cnds;
-               ?>],&quot;datasets&quot;:[{&quot;label&quot;:&quot;&quot;,&quot;backgroundColor&quot;:[<?php
-                 $arr_length = count($votesarr);
-                 $colors = '';
-                 for ($i=0; $i < $arr_length; $i++) {
-                   $colors = $colors.'&quot;'.sprintf('#%06X', mt_rand(0, 0xFFFFFF)).'&quot;,';
-                 }
-                 $colors = substr($colors, 0, -1);
-                 echo $colors;
-                 ?>],&quot;borderColor&quot;:[<?php
-                 //$arr_length = count($votesarr);
-                 $fff = '';
-                 for ($i=0; $i < $arr_length; $i++) {
-                   $fff = $fff.'&quot;#ffffff&quot;,';
-                 }
-                 $fff = substr($fff, 0, -1);
-                 echo $fff;
-                 ?>],&quot;data&quot;:[<?php
-                 $vts = '';
-                 foreach ($votesarr as $candidate => $votes) {
-                   $vts = $vts.'&quot;'.$votes.'&quot,';
-                 }
-                 $vts = substr($vts, 0, -1);
-                 echo $vts;
-                  ?>]}]},&quot;options&quot;:{&quot;maintainAspectRatio&quot;:false,&quot;legend&quot;:{&quot;display&quot;:true, &quot;position&quot;:&quot;bottom&quot;},&quot;title&quot;:{}}}" width="668" height="352" class="chartjs-render-monitor" style="display: block; width: 608px; height: 320px;"></canvas>
+            <canvas id="doughnut1" width="668" height="352" style="display: block; width: 608px; height: 320px;" class="chartjs-render-monitor"></canvas>
+
+
         </div>
-
+<div class="custom-legend doughnut-meter-legend clearfix d-none d-lg-block"></div>
+<div class="custom-legend custom-legend-2 doughnut-meter-legend clearfix d-lg-none"></div>
   </div>
 </div>
 </div>
 
   </div>
 </div>
+
 
  <?php
 include 'inc/footer.php';
   ?>
+
+  <script type="text/javascript">
+  var doughnutElm = document.getElementById('doughnut1');
+  var doughnut1data = [<?php
+  $vts = '';
+  foreach ($votesarr as $candidate => $votes) {
+    $vts = $vts.$votes.", ";
+  }
+  $vts = substr($vts, 0, -2);
+  echo $vts;
+   ?>];
+  var cloneData = doughnut1data.slice(0);
+  var doughnut1dataset = {
+    labels: [
+      <?php
+        $cnds = '';
+        foreach ($votesarr as $candidate => $votes) {
+          $cnds = $cnds."'".$candidate."',";
+        }
+        $cnds = substr($cnds, 0, -1);
+        echo $cnds;
+         ?>
+      ],
+    datasets: [
+      {
+        data: doughnut1data,
+        backgroundColor: [
+          <?php
+            $arr_length = count($votesarr);
+            $colors = '';
+            for ($i=0; $i < $arr_length; $i++) {
+              $colors = $colors."'".sprintf('#%06X', mt_rand(0, 0xFFFFFF))."',";
+            }
+            $colors = substr($colors, 0, -1);
+            echo $colors;
+            ?>
+              ],
+        hoverBorderWidth: 0
+                      }]
+  };
+  var doughnutOptions = {
+    maintainAspectRatio: false,
+    responsive: true,
+    legend: {
+      display: false,
+      labels: {
+        usePointStyle: true
+      }
+    },
+  };
+
+  if (doughnutElm) {
+    var doughnut1 = doughnutElm.getContext('2d');
+    var myDoughnutChart = new Chart(doughnut1, {
+      type: 'doughnut',
+      data: doughnut1dataset,
+      options: doughnutOptions
+    });
+    $('.doughnut-meter-legend').append(myDoughnutChart.generateLegend());
+    $('.doughnut-meter-legend li').each(function () {
+      $(this).append(': <b>' + cloneData[$(this).index()] + '</b>')
+    });
+    $('.doughnut-meter-legend').on('click', 'li', function () {
+      myDoughnutChart.data.datasets[0].data[$(this).index()] = cloneData[$(this).index()] !== myDoughnutChart.data.datasets[0].data[$(this).index()] ? cloneData[$(this).index()] : 0;
+      myDoughnutChart.update();
+      $(this).toggleClass('striked');
+    });
+  };
+  </script>
--- inc/header.php
+++ inc/header.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
     <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 
 <body id="page-top">
