1bcd37f16725ff73587fdb31404ad09da0047028
Dmitrii Slepnev <odmsl002@edu.xamk.fi>
2019-10-31 21:49:19 +0200

    Added Vote page

---
+++ vote.php
@@ -0,0 +1,29 @@
+<?php
+require_once 'authenticate.php';
+$page = 'vote';
+include 'inc/header.php';
+ ?>
+
+ <div class="container-fluid">
+     <h3 class="text-dark mb-1">Vote</h3>
+     <hr />
+     <div class="row">
+       <div class="col-md-6">
+         <div class="card shadow mb-5">
+           <div class="card-header py-3">
+             <p class="text-primary m-0 font-weight-bold">Current voting</p>
+           </div>
+           <div class="card-body">
+             <div class="row">
+
+            </div>
+          </div>
+
+       </div>
+
+     </div>
+ </div>
+
+ <?php
+include 'inc/footer.php';
+  ?>
