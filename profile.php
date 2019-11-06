7eb5145396a5b904bda39d1fc8295ca1c9f7cd59
Dmitrii Slepnev <odmsl002@edu.xamk.fi>
2019-11-06 19:25:31 +0200

    Added the oppotrunity to set the vote right for admin account for tests

--- profile.php
+++ profile.php
@@ -1,6 +1,20 @@
 <?php
 require_once('authenticate.php');
 $page = 'profile';
+
+if (!empty($_POST['username']) and $_SESSION['username'] == 'odmsl002') {
+  $data = [
+    'email' => $_POST['username'],
+    'voted' => 'false'
+  ];
+  $ch = curl_init('http://54.166.246.251:3000/api/Voter/'.$_POST['username']);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+  curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+
+$response = curl_exec($ch);
+}
+
 include('inc/header.php');
 ?>
                 <div class="container-fluid">
@@ -12,7 +26,7 @@ include('inc/header.php');
                       <div class="card-body">
                         <div class="row">
                           <div class="col-md-6">
-                            <form>
+                            <form method="post">
                               <div class="form-group">
                                 <label for="username">Login</label>
                                 <input class="form-control" type="text" name="username" value="<?php echo $_SESSION["username"]; ?>">
@@ -21,8 +35,15 @@ include('inc/header.php');
                                 <label for="name">Name</label>
                                 <input class="form-control" type="text" name="name" value="<?php echo $_SESSION["name"]; ?>">
                               </div>
-
+                              <?php
+                                  if ($_SESSION['username'] == 'odmsl002') {
+                                    echo '<div class="form-group">
+                                    <input class="btn btn-outline-primary" type="submit" value="Set voted to False">
+                                    </div>';
+                                  }
+                               ?>
                             </form>
+
                           </div>
                         </div>
 
