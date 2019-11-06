f9223248e44924eaf17822b01006fb88c3ac4f74
Dmitrii Slepnev <odmsl002@edu.xamk.fi>
2019-11-06 19:02:41 +0200

    MakeVote transaction execution added to vote.php

--- inc/footer.php
+++ inc/footer.php
@@ -10,6 +10,7 @@
 <script src="assets/js/chart.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.js"></script>
 <script src="assets/js/script.min.js"></script>
+<script src="assets/js/dismiss.js"></script>
 </body>
 
 </html>
--- vote.php
+++ vote.php
@@ -1,9 +1,8 @@
 <?php
 require_once 'authenticate.php';
 $page = 'vote';
-include 'inc/header.php';
 
-$candidatesarr = array("Obama", "Trump", "Putin", "Medvedev", "Johnson", "Zelensky");
+//$candidatesarr = array("Obama", "Trump", "Putin", "Medvedev", "Johnson", "Zelensky");
 
 //Obtaining our voter data from the blockchain
 // Get cURL resource
@@ -30,6 +29,57 @@ if (!$json) {
     $voted_bool = false;
   }
 }
+
+//Obtaining our candidates data from the blockchain
+// Get cURL resource
+$curl = curl_init();
+// Set some options - we are passing in a useragent too here
+curl_setopt_array($curl, [
+    CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_URL => 'http://54.166.246.251:3000/api/Candidate',
+]);
+// Send the request & save response to $resp
+$resp = curl_exec($curl);
+// Close request to clear up some resources
+curl_close($curl);
+//Decoded result:
+$candidatesarr = json_decode($resp);
+
+if (!empty($_GET['votefor'])) {
+  $votefor = $_GET['votefor'];
+  $valid = false;
+  foreach ($candidatesarr as $candidate) {
+    if ($candidate -> {'name'} == $votefor) {
+      $valid = true;
+    }
+  }
+  if ($valid and !$voted_bool) {
+      $post = [
+                'voter' => $_SESSION['username'],
+                'candidate' => $votefor,
+            ];
+      $headers = [
+        'Content-Type: application/json',
+        'Accept: application/json'
+      ];
+
+      $ch = curl_init('http://54.166.246.251:3000/api/MakeVote');
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_POST, 1);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post));
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+      // execute!
+      $response = curl_exec($ch);
+      // close the connection, release resources used
+      curl_close($ch);
+
+      header('Location: vote.php');
+
+  }
+}
+
+
+include 'inc/header.php';
  ?>
 
  <div class="container-fluid">
@@ -37,15 +87,31 @@ if (!$json) {
      <hr />
      <div class="row">
        <div class="col-md-8">
+         <?php
+         if ($votefor and !$valid) {
+             echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">
+             No such candidate!
+             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+             <span aria-hidden="true">&times;</span>
+             </button>
+             </div>';
+         }
+
+         //echo var_export($post);
+         //echo "<br/>";
+         //echo var_export($response);
+          ?>
          <div class="card shadow mb-5">
            <div class="card-header py-3">
              <p class="text-primary m-0 font-weight-bold">Current voting</p>
            </div>
            <div class="card-body">
              <div class="row">
+
                <?php if ($voted_bool) {
                  echo "<p>You have already voted. <a href=\"candidates.php\">See current results.</a></p>";
-               } ?>
+               }
+                ?>
                <table class="table">
        <thead>
          <tr>
@@ -60,11 +126,11 @@ if (!$json) {
              foreach ($candidatesarr as $candidate) {
                echo "<tr>";
                echo '<th scope="row">'.$num.'</th>';
-               echo '<td>'.$candidate.'</td>';
+               echo '<td>'.$candidate -> {'name'}.'</td>';
                if ($voted_bool) {
-                 echo '<td class="text-center"><button type="button" class="btn btn-primary btn-sm" disabled>Vote for '.$candidate.'!</button></td>';
+                 echo '<td class="text-center"><button type="button" class="btn btn-primary btn-sm" disabled>Vote for '.$candidate -> {'name'}.'!</button></td>';
                } else {
-               echo '<td class="text-center"><button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#voteModal'.$num.'">Vote for '.$candidate.'!</button></td>';
+               echo '<td class="text-center"><button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#voteModal'.$num.'">Vote for '.$candidate -> {'name'}.'!</button></td>';
 
                echo '<div class="modal fade" id="voteModal'.$num.'" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
@@ -76,11 +142,11 @@ if (!$json) {
         </button>
       </div>
       <div class="modal-body">
-        Are you sure that you want to vote for '.$candidate.'?
+        Are you sure that you want to vote for '.$candidate -> {'name'}.'?
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-danger px-5" data-dismiss="modal">No</button>
-        <button type="button" class="btn btn-success px-5">Yes</button>
+        <a href="vote.php?votefor='.$candidate -> {'name'}.'"><button type="button" class="btn btn-success px-5">Yes</button></a>
       </div>
     </div>
   </div>
