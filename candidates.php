1008a6b129e26361bad5195eb6fc9ed95ee75776
Dmitrii Slepnev <odmsl002@edu.xamk.fi>
2019-11-06 13:58:13 +0200

    Candidates are now loaded from the blockchain

--- candidates.php
+++ candidates.php
@@ -3,10 +3,26 @@ require_once 'authenticate.php';
 $page = 'candidates';
 include 'inc/header.php';
 
-$votesarr = array("Obama" => 10, "Trump" => 20, "Putin" => 30, "Medvedev" => 6, "Johnson" => 18, "Zelensky" => 3);
+//$votesarr = array("Obama" => 10, "Trump" => 20, "Putin" => 30, "Medvedev" => 6, "Johnson" => 18, "Zelensky" => 3);
+
+//Obtaining our candidates data from the blockchain
+// Get cURL resource
+$curl = curl_init();
+// Set some options - we are passing in a useragent too here
+curl_setopt_array($curl, [
+    CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_URL => 'http://54.166.246.251:3000/api/Candidate',
+]);
+// Send the request & save response to $resp
+$resp = curl_exec($curl);
+// Close request to clear up some resources
+curl_close($curl);
+//Decoded result:
+$votesarr = json_decode($resp);
+
 $sum = 0;
-foreach ($votesarr as $candidate => $votes) {
-$sum += $votes;
+foreach ($votesarr as $item) {
+$sum += $item -> {'votes'};
 }
 
  ?>
@@ -36,13 +52,13 @@ $sum += $votes;
   <tbody>
     <?php
         $num = 1;
-        foreach ($votesarr as $candidate => $votes) {
+        foreach ($votesarr as $candidate) {
           echo "<tr>";
           echo '<th scope="row">'.$num.'</th>';
-          echo '<td>'.$candidate.'</td>';
-          echo '<td>'.$votes.'</td>';
+          echo '<td>'.$candidate -> {'name'}.'</td>';
+          echo '<td>'.$candidate -> {'votes'}.'</td>';
           echo '<td><div class="progress">
-      <div class="progress-bar" role="progressbar" aria-valuenow="'.round($votes/$sum*100).'" aria-valuemin="0" aria-valuemax="100" style="width:'.round($votes/$sum*100).'%">'.round($votes/$sum*100).'%</div>
+      <div class="progress-bar" role="progressbar" aria-valuenow="'.round($candidate -> {'votes'}/$sum*100).'" aria-valuemin="0" aria-valuemax="100" style="width:'.round($candidate -> {'votes'}/$sum*100).'%">'.round($candidate -> {'votes'}/$sum*100).'%</div>
     </div></td>';
           $num++;
         }
@@ -88,8 +104,8 @@ include 'inc/footer.php';
   var doughnutElm = document.getElementById('doughnut1');
   var doughnut1data = [<?php
   $vts = '';
-  foreach ($votesarr as $candidate => $votes) {
-    $vts = $vts.$votes.", ";
+  foreach ($votesarr as $candidate) {
+    $vts = $vts.$candidate -> {'votes'}.", ";
   }
   $vts = substr($vts, 0, -2);
   echo $vts;
@@ -99,8 +115,8 @@ include 'inc/footer.php';
     labels: [
       <?php
         $cnds = '';
-        foreach ($votesarr as $candidate => $votes) {
-          $cnds = $cnds."'".$candidate."',";
+        foreach ($votesarr as $candidate) {
+          $cnds = $cnds."'".$candidate -> {'name'}."',";
         }
         $cnds = substr($cnds, 0, -1);
         echo $cnds;
