e1698c1f1481e1730092a8693770eae3040baaf0
Dmitrii Slepnev <odmsl002@edu.xamk.fi>
2019-11-14 16:27:44 +0200

    Improved main page

--- index.php
+++ index.php
@@ -8,7 +8,13 @@ include('inc/header.php');
                     <h3 class="text-dark mb-1">Blockchain Voting Application</h3>
                     <hr />
                     <h4 class="text-dark mb-1">Welcome, <?php echo $_SESSION["name"]; ?>!</h3>
-                      <p>I am planning to put the main content of my application here.</p>
+                      <p>This application is based on blockchain.</p>
+                      <p>Using blockchain for voting app provides the following benefits:</p>
+                      <ul>
+                        <li>resistance to the illegal data modification</li>
+                        <li>immutable and distributed transaction history</li>
+                        <li>smart contracts which can add complex logic to the transactions</li>
+                      </ul>
                       <p><?php echo $_SESSION["test"]; ?></p>
                 </div>
 
